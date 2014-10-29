3899599a19bc1efd66a4b9fb6bc6747939b30ad3
Janith Perera <janith@localhost.localdomain>
2014-10-29 19:45:07 +0545

    GPL added

--- oramx.pl
+++ oramx.pl
@@ -1,3 +1,29 @@
+#<Oramx converts oracle to sqlmx.>
+#Copyright (C) <2014> <Janith Perera>
+
+#------------------------------------------------------------------------------
+# Name     : Oramx/oramx.pl
+# Language : Perl
+# Authors  : Janith Perera, janith@member.fsf.org
+# Function : Main program
+# Usage    : See documentation
+#------------------------------------------------------------------------------
+
+#This file is part of Oramx.
+
+#Oramx is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#Oramx is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Oramx. If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/perl -w                                                                                                                                           
 use Config::Simple;
 use strict;
