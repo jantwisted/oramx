bcad6bd163a42718f1424fec38453407fcc6f377
Janith Perera <janith@localhost.localdomain>
2014-10-29 13:33:18 +0545

    primary key conversion added

--- oramx.pl
+++ oramx.pl
@@ -42,30 +42,82 @@ sub get_dbconnection{
 sub main{
     get_dbconnection();
     my $sql = 'SELECT column_name, data_type, data_length, nullable  FROM USER_TAB_COLUMNS WHERE table_name = ?';
+    my $pksql = 'SELECT cols.column_name FROM all_constraints cons, all_cons_columns cols WHERE cols.table_name = ? AND cons.constraint_type = ? AND cons.constraint_name = cols.constraint_name AND cons.owner = cols.owner AND cols.owner = ?';
+    my $pknamesql = 'SELECT cols.constraint_name FROM all_constraints cons, all_cons_columns cols WHERE cols.table_name = ? AND cons.constraint_type = ? AND cons.constraint_name = cols.constraint_name AND cons.owner = cols.owner AND cols.owner = ?';
     my $sth = $dbh->prepare($sql);
     my @list;
+    my @pklist;
+    my @pkname;
     $sth->execute($db_object);
+
+# get primary key
+    my $sthpk = $dbh->prepare($pksql);
+    $sthpk->execute($db_object, 'P', $db_schema);
+
+# get constraint name
+    my $sthpkname = $dbh->prepare($pknamesql);
+    $sthpkname->execute($db_object, 'P', $db_schema);
+
     while (my @row = $sth->fetchrow_array) {
 	push @list, @row;
     }
-    table_constructor(@list);
+    while (my @row_pk = $sthpk->fetchrow_array){
+	push @pklist, @row_pk;
+    }
+    while (my @row_pk_name = $sthpkname->fetchrow_array){
+	push @pkname, @row_pk_name;
+    }
+    table_constructor(\@list, \@pklist, \@pkname);
     $dbh->disconnect();
     
 }
 
-sub get_primary{
+sub primary_key_constraint{
+    my ($pkcolumns, $name) = @_;
+    my $_name = '';
+    my $pk_query = '';
+    foreach(@$name){ $_name = $_; }
+    $pk_query = "\n\tCONSTRAINT $_name PRIMARY KEY (";
+    for(@$pkcolumns){
+	$pk_query .= $_." ASC";
+	if(  \$_ == \$$pkcolumns[-1]  ) {
+	    $pk_query .= ") NOT DROPPABLE";
+	}else{
+	    $pk_query .= ', ';
+	}
+    }
+    return $pk_query;
+
+}
+
+
+sub primary_key_footer{
+    my ($pkcolumns) = @_;
+    my $pk_query = '';
+    $pk_query = "\nSTORE BY(";
+    for(@$pkcolumns){
+	$pk_query .= $_." ASC";
+	if(  \$_ == \$$pkcolumns[-1]  ) {
+	    $pk_query .= ")";
+	}else{
+	    $pk_query .= ', ';
+	}
+    }
+    return $pk_query;
     
 }
 
+
 sub table_constructor{
+
+    my ($columns, $pk, $pkn) = @_;
     my $ddl_head = "CREATE TABLE $db_object\n(";
-    my $ddl_tail = "\n\t)\n";
+    my $ddl_tail = "\n)\n";
     my $i = 0;
     my $con = 0;
-    print $ddl_head;
     my $master_ddl = '';
-
-    for(@_){
+    my $meta = "LOCATION ".$location."\nATTRIBUTES BLOCKSIZE ".$blocksize.", EXTENT(".$extent1.", ".$extent2."), MAXEXTENTS ".$maxextents;
+    for(@$columns){
 	if ($i==0){
 	    $master_ddl = $master_ddl."\n\t".$_." ";
 	    $i++;
@@ -88,11 +140,16 @@ sub table_constructor{
 	    $i++;
 	    next;
 	}else{
-	    if(  \$_ == \$_[-1]  ) {
+	    if(  \$_ == \$$columns[-1]  ) {
 		if ($_ eq 'N'){
 		    $master_ddl = $master_ddl." NOT NULL";
 		}
-
+		if (@$pk){
+		    $master_ddl .= ','.primary_key_constraint(\@$pk, \@$pkn);
+		    $master_ddl = $ddl_head.$master_ddl.$ddl_tail.$meta.primary_key_footer(\@$pk).";";
+		}else{
+		    $master_ddl = $ddl_head.$master_ddl.$ddl_tail.$meta.";";
+		}
 	    }else{
 		if ($_ eq 'N'){
 		    $master_ddl = $master_ddl." NOT NULL,";
@@ -104,8 +161,8 @@ sub table_constructor{
 	    next;
 	}
     }
-    my $meta = "LOCATION ".$location."\nATTRIBUTES BLOCKSIZE ".$blocksize.", EXTENT(".$extent1.", ".$extent2."), MAXEXTENTS ".$maxextents.";";
-    $master_ddl = $master_ddl.$ddl_tail.$meta;
+    
+    
     print $master_ddl;
 }
 
